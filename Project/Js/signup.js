0f27bf8506d6d6bb1522315a5eaab9abbb39a606
JassSangha <elegantjassa+github@gmail.com>
2022-11-13 21:42:57 +0530

    timer works now

--- Project/Js/signup.js
+++ Project/Js/signup.js
@@ -19,19 +19,49 @@ function togglepassword() {
 } 
 
 // 1 Min Countdown
-
-
-
-
+let second = 5;
+let millisecond = 0;
+let cron;
+document.login_form.otpbtn.onclick = () => start();
+  
+function start() {
+  cron = setInterval(() => { timer(); }, 10);
+}
+function timer() {
+  if ((millisecond += 10) == 1000) {
+    millisecond = 0;
+    second--;
+  }
+  if (second >=0 ){
+    document.getElementById('second').innerText = returnData(second);
+    document.querySelector('#otp-btn').textContent = "";
+    document.querySelector('#otp-btn').disabled = true;
+    // document.querySelector('#message').textContent = 
+    //   "Otp has been sent to your mobile or email";
+  }
+  if (second == 0) {
+    document.querySelector('#second').textContent = "";
+    clearInterval(cron);
+    second = 5;
+    document.getElementById("timer").innerHTML = "";
+    document.querySelector('#otp-btn').textContent = "Resend Otp";
+    document.querySelector('#otp-btn').disabled = false;
+    // document.querySelector('#message').textContent = 
+    //   "";
+  }
+  function returnData(input) {
+    return input > 10 ? input : `0${input}`
+  }
+}
 
 
 // queries
 
 // if (seconds > 0) {
+  //   document.querySelector('#otp-btn').textContent = "";
+  //   document.querySelector('#message').textContent = 
+  //     "Otp has been sent to your mobile or email";
 //   setTimeout(tick, 1000);
-//   document.querySelector('#otp-btn').textContent = "";
-//   document.querySelector('#message').textContent = 
-//     "Otp has been sent to your mobile or email";
 // }
 // else {
 //   document.querySelector('#message').textContent = 
