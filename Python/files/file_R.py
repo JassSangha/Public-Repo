171cc75bd0e9cdf4592e8766c84bf5465062afab
Bajaj-Muskan <bajajmuskan2002@gmail.com>
2022-11-15 14:15:55 +0530

    sanju file

--- Python/files/file_R.py
+++ Python/files/file_R.py
@@ -1,17 +1,20 @@
-f= open('jassa.txt','rt')
-content=f.read(10)
-print(content)
-content=f.read(20)
-print(content)
-content=f.read()
-print(content)
+f= open("jassa.txt",'rt')
 
-# print(f.readline(),end='')
-# print(f.readline(),end='')
-# print(f.readlines())
+# content=f.read(10)
+# print(content)
+# content=f.read(20)
+# print(content)
+# content=f.read()
+# print(content)
+
+print(f.readline(),end='')
+print(f.readline(),end='')
+print(f.readlines())
 
 
 # for i in f:
 #     print(i,end='')
 # f.close()
+
+
 
